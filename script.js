b96f740d0784d5574d3b430fee3c82ee236992ea
cheryy3007 <zulfiyagazna@gmail.com>
2024-11-16 19:34:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let oprosname = {}
+
+oprosname.name = prompt('Введите ваше имя');
+oprosname.id = prompt('Введите ваш id в MARS SPACE');
+oprosname.password = prompt('Введите ваш пароль в MARS SPACE');
+
+
+console.log(oprosname);
